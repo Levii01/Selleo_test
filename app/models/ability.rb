e822fff957ef44ac051aae434b71d7c5b88528a3
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-09 21:16:37 +0100

    Prepare controllers and views for admins and create abilitys for them

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
 
+    if user.admin?
+      can :manage, :all
+      cannot :destroy, User, id: user.id
+    end
   end
 end
