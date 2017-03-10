79c0a9f513c2aa6be4d21624bb6bf78aedfde0ed
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-03-10 08:23:54 +0100

    Spec for models interests, user and for users controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,39 @@ RSpec.describe User, type: :model do
 
   describe 'create new user' do
     context 'when creating new user' do
-      subject { build :user, email: 'model@test.com' }
-
+      subject { build :user, email: 'model@test.com', gender: nil }
       it { is_expected.to be_valid }
     end
 
-    context "when user with that email is already exist" do
-      let(:another_user) { create :user , email: "test@test.com"}
+    context 'when gender is set to nil' do
+      subject { create :user, email: 'model@test.com', gender: nil }
+      it { expect(subject.reload.gender).to eq('Not defined') }
+    end
+    context 'when gender is set to male' do
+      subject { create :user, email: 'model@test.com', gender: 'Male' }
+      it { expect(subject.reload.gender).to eq('Male') }
+    end
+
+    context 'when gender is not valid' do
+      subject { build :user, email: 'model@test.com', gender: 'Potato' }
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'when user with that email is already exist' do
+      let(:another_user) { create :user , email: 'test@test.com'}
       subject { build :user, another_user.attributes }
 
       it { is_expected.to_not be_valid }
     end
 
-    context 'when try to create empty user' do
+    context 'when trying to create empty user' do
       subject { build :user, email: '', password: '' }
 
       it { is_expected.to_not be_valid }
+    end
+
+    describe 'associations' do
+      it { is_expected.to have_many :interests }
     end
   end
 end
